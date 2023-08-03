df307cd9e69542d3d00734d75d388bc87200be02
Vítor Gabriel <vggd@cin.ufpe.br>
2023-08-02 21:23:27 -0300

    update SE1

--- [2021 rev] Componentes do Projeto/Componentes do Projeto/sign_extended1bit.v
+++ [2021 rev] Componentes do Projeto/Componentes do Projeto/sign_extended1bit.v
@@ -1,8 +1,9 @@
 module sign_extended1bit(
-            input wire [15:0] input_data,
+            input wire input_data,
             output wire [31:0] out_data
             );
+            
     // replica o bit de sinal
-    assign out_data =  {{31{1'b0}}, input_data}
+    assign out_data = input_data[15 ? {{31{1'b1}}, input_data} : {{31{1'b0}}, input_data}
 
 endmodule
