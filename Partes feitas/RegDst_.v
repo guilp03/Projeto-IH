c4c1a0ca85a4909c709ed8b539f8cd9c88613c96
Eduardovdcn <72761294+Eduardovdcn@users.noreply.github.com>
2023-08-04 13:40:45 -0300

    Correção RegDst

--- Partes feitas/RegDst_.v
+++ Partes feitas/RegDst_.v
@@ -6,10 +6,8 @@ module  RegDst_(
     input wire [15:0] OFFSET,
     output reg [4:0] RegDst_out
 );
-    
 
-
-    always @(*)begin
+    always @(RegDstControl or ConstVinte_nove or RT or ConstTrinta_um or OFFSET)begin
         case(RegDstControl)
             2'b00 : RegDst_out = ConstVinte_nove;
             2'b01 : RegDst_out = RT;
@@ -17,3 +15,4 @@ module  RegDst_(
             2'b11 : RegDst_out = OFFSET[15:11];
         endcase
     end
+endmodule
