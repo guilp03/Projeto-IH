ca9b8e1dff27ec9d2d941acac56a48826ec4b46f
Vítor Gabriel <vggd@cin.ufpe.br>
2023-08-05 14:47:57 -0300

    Update sign_extended1bit.v

--- Partes feitas/sign_extended1bit.v
+++ Partes feitas/sign_extended1bit.v
@@ -2,8 +2,6 @@ module sign_extended1bit(
             input wire input_data,
             output wire [31:0] out_data
             );
-            
-    // replica o bit de sinal
-    assign out_data = {31'b0, input_data}
 
-endmodule
+    assign out_data = {{31{input_data}}, input_data};
+endmodule   
