a932e10e32d102df841932ae1740f6ce1aaf31e3
Vítor Gabriel <vggd@cin.ufpe.br>
2023-08-05 14:30:08 -0300

    at sign extended

--- Partes feitas/sign_extended1bit.v
+++ Partes feitas/sign_extended1bit.v
@@ -4,6 +4,6 @@ module sign_extended1bit(
             );
             
     // replica o bit de sinal
-    assign out_data = input_data[15 ? {{31{1'b1}}, input_data} : {{31{1'b0}}, input_data}
+    assign out_data = {{31{1'b0}}, input_data}
 
 endmodule
